611601ed7af65e5b65232b5afd3e1e83ba0488a3
Nicki Dorrmann <nicki.dorrmann@googlemail.com>
2022-02-15 20:05:15 +0100

    added method LUT generation

--- src/memory.rs
+++ src/memory.rs
@@ -9,6 +9,7 @@ use std::collections::HashMap;
 
 const INIT_SIG : &str = "<init>()V";
 const MAIN_SIG : &str = "main([Ljava/lang/String;)V";
+const LUTENTRY : u8   = 3;
 
 ///
 /// Creates memory layout for Bali program memory out of a JVM class file.
@@ -44,6 +45,22 @@ pub fn memlayout(classinfo : &ClassFile) -> (BiBTreeMap<u16, String>, u16) {
     (methodaddrs, currentaddr)
 }
 
+fn methodlut(classinfo : &ClassFile) -> Vec<u8> {
+    let codeblocks = codeblocks(&classinfo);
+    let (memlayout, _) = memlayout(&classinfo);
+    let lutsize = LUTENTRY * memlayout.len() as u8;
+
+    let mut methodlut : Vec<u8> = Vec::with_capacity(lutsize.into());
+
+    for (methodaddr, methodname) in memlayout {
+        methodlut.push(((lutsize as u16 + methodaddr as u16) >> 8) as u8);
+        methodlut.push(((lutsize as u16 + methodaddr as u16) & 0xff) as u8);
+        methodlut.push(codeblocks[&methodname].max_locals.try_into().unwrap());
+    }
+
+    methodlut
+}
+
 ///
 /// Generate binary stream to write to Bali processor program memory.
 ///
@@ -51,22 +68,26 @@ pub fn memlayout(classinfo : &ClassFile) -> (BiBTreeMap<u16, String>, u16) {
 ///
 pub fn binarygen(classinfo : &ClassFile) -> Vec<u8> {
     let methodrefs = methodrefs(&classinfo);
-    let memlayout = memlayout(&classinfo);
+    let (memlayout, codesize) = memlayout(&classinfo);
+    let mut methodlut = methodlut(&classinfo);
     let opmap = opmap();
 
     // map method reference index to Bali program memory address
     let mut refaddr : HashMap<u16, u16> = HashMap::new();
     for (methodref, methodname) in methodrefs {
         if methodname == INIT_SIG { continue; }
-        refaddr.insert(methodref, *memlayout.0.get_by_right(&methodname).unwrap());
+        refaddr.insert(methodref, *memlayout.get_by_right(&methodname).unwrap());
     }
 
     // replace invokestatic address arguments with Bali memory addresses
     let codeblocks = codeblocks(&classinfo);
-    let mut mem = Vec::with_capacity(memlayout.1.into());
-    for (_, methodname) in memlayout.0 {
-        let code_old = codeblocks[&methodname].code.to_vec();
-        let mut code_new = codeblocks[&methodname].code.to_vec();
+    let memsize = methodlut.len() as u16 + codesize;
+    let mut mem = Vec::with_capacity(memsize.into());
+    mem.append(&mut methodlut);
+
+    for (pos, (_, methodname)) in memlayout.iter().enumerate() {
+        let code_old = codeblocks[methodname].code.to_vec();
+        let mut code_new = codeblocks[methodname].code.to_vec();
 
         let mut argcount = 0;
         for (i, opcode) in code_old.iter().enumerate() {
@@ -80,9 +101,7 @@ pub fn binarygen(classinfo : &ClassFile) -> Vec<u8> {
             argcount = op.args;
 
             if op.mnemonic == "invokestatic" {
-                let arg = (code_old[i + 1] as u16) << 8 | (code_old[i + 2] as u16);
-                let newref = refaddr[&arg];
-
+                let newref = pos as u16;
                 code_new[i + 1] = (newref >> 8) as u8;
                 code_new[i + 2] = (newref & 0xff) as u8;
             }
