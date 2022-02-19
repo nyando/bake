64124325ed3b7dd3cfb41ba0da86f3e7efd94f80
Nicki Dorrmann <nicki.dorrmann@googlemail.com>
2022-02-19 20:59:44 +0100

    added uart communication

--- src/uart.rs
+++ src/uart.rs
@@ -1,25 +1,34 @@
 /// uart.rs - UART communication for Bali
 
 extern crate serialport;
-use serialport::{SerialPort};
+use serialport::{DataBits, FlowControl, Parity, StopBits};
 
-fn open() {
+use std::time::Duration;
 
-    ///
-    /// open a UART connection to COM or TTY port for the FPGA
-    /// configuration:
-    ///  - baudrate: 9600
-    ///  - data bits: 8
-    ///  - parity bit: none
-    ///  - flow control: none
-    ///
-    /// handshake? (would need to be implemented in SV too)
-    ///
+pub fn open_serial(port_id : &str) -> std::io::Result<()> {
+    let mut port = serialport::new(port_id, 9600)
+        .data_bits(DataBits::Eight)
+        .flow_control(FlowControl::None)
+        .parity(Parity::None)
+        .stop_bits(StopBits::One)
+        .timeout(Duration::from_millis(100))
+        .open()
+        .expect("could not open serial port");
 
-}
+    for i in 0..255 {
+        let input : Vec<u8> = vec!(i);
 
-fn write(mem: Vec<u8>) {
+        port.write_all(&input)?;
+        println!("wrote {:#04x} to serial port", i);
+        
+        let mut buffer : Vec<u8> = vec![0; 1];
+        port.read(&mut buffer)?;
 
-    /// send all bytes in mem
+        for byte in buffer {
+            println!("received value {:#04x} on serial port", byte);
+        }
 
+    }
+
+    Ok(())
 }
